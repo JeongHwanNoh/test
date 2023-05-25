a39ca11b45499337a81ae7bbeae320194c2e4085
jeonghwannoh <jeonghwan123456789@gmail.com>
2023-05-25 13:22:37 +0900

    5월 25일 백준 1085번

---
+++ src/main/java/sqrun.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+    public class sqrun {
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+            int d = in.nextInt();
+
+            int x = Math.min(a, c-b);
+            int y = Math.min(b, d-b);
+            
+            System.out.println(Math.min(x, y));
+        }
+
+    }
+
