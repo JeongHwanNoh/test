9a21b0cc74c4a2ac07824b774cdfd7bbd6cebadc
jeonghwannoh <jeonghwan123456789@gmail.com>
2023-06-15 22:15:28 +0900

    6월 15일

---
+++ src/main/java/back10926.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class back10926 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+
+        String lastString = "??!";
+
+        String s = b.readLine();
+        System.out.println(s + lastString);
+    }
+}
