11a09792d12e8ac07efa782e9f17c977cc78936e
jeonghwannoh <jeonghwan123456789@gmail.com>
2023-08-10 21:17:17 +0900

    8월 10일

---
+++ src/main/java/display/Menu.java
@@ -0,0 +1,8 @@
+package display;
+
+import display.Display;
+public class Menu {
+    public static void main(String[] args) {
+        new Display("메뉴",1200,800);
+    }
+}
---
+++ src/main/java/pratice0724.java
@@ -0,0 +1,5 @@
+public class pratice0724 {
+    public static void main(String[] args) {
+
+    }
+}
